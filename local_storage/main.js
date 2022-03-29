f90882369bce6188e04af3e6ab6eeeb873218747
Lakshit-Chiranjiv <luckyhacker017@gmail.com>
2022-03-29 19:55:26 +0530

    storing objects using localstorage

--- local_storage/main.js
+++ local_storage/main.js
@@ -4,3 +4,12 @@ localStorage.setItem('name','henry');
 localStorage.setItem('score',72);
 console.log(localStorage.getItem('name')+' scored '+localStorage.getItem('score'));
 localStorage.removeItem('score');
+
+const exampleObj = {
+    name: 'Lakshit',
+    age: 20,
+    role: 'developer'
+}
+
+localStorage.setItem('exObj',JSON.stringify(exampleObj));
+console.log(JSON.parse(localStorage.getItem('exObj')));
